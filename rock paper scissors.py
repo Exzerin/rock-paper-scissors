9e5cb6dbce34ebd21282fc98c39027e0d1cb1fbb
Exzerin <exzerin@gmail.com>
2021-04-22 21:25:25 -0400

    Update rock paper scissors.py

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,52 +1,50 @@
-import random
-import math
+import random 
+import math 
 
 def play():
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    user = user.lower()
-
-    computer = random.choice(['r', 'p', 's'])
-
+    
+    user = input("ROCK, PAPER, or SCISSORS?\n")
+    user = user.upper()
+    
+    computer = random.choice(['ROCK', 'PAPER', 'SCISSORS'])
+    
     if user == computer:
         return (0, user, computer)
-
-    # r > s, s > p, p > r
+    
     if is_win(user, computer):
         return (1, user, computer)
-
+    
     return (-1, user, computer)
 
 def is_win(player, opponent):
-    # return true is the player beats the opponent
-    # winning conditions: r > s, s > p, p > r
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+    # return true if the paper beats the opponnent 
+    # winning conditions rock > scissors, scissors > paper, paper > rock
+    if (player == "ROCK" and opponent == "SCISSORS") or (player == "SCISSORS" and opponent == "PAPER") or (player == "PAPER" and opponent == "ROCK"):
         return True
-    return False
+    return False 
 
 def play_best_of(n):
-    # play against the computer until someone wins best of n games
-    # to win, you must win ceil(n/2) games (ie 2/3, 3/5, 4/7)
+    # play agaisnt pc until someone wins best of n games
     player_wins = 0
     computer_wins = 0
     wins_necessary = math.ceil(n/2)
     while player_wins < wins_necessary and computer_wins < wins_necessary:
         result, user, computer = play()
-        # tie
+        # tie 
         if result == 0:
-            print('It is a tie. You and the computer have both chosen {}. \n'.format(user))
-        # you win
+            print('It is a tie. You both chose {}.\n'.format(user))
+        # win
         elif result == 1:
             player_wins += 1
             print('You chose {} and the computer chose {}. You won!\n'.format(user, computer))
         else:
             computer_wins += 1
-            print('You chose {} and the computer chose {}. You lost :(\n'.format(user, computer))
-
+            print('You chose {} and the computer chose {}. You lost..\n'.format(user, computer))
+        
     if player_wins > computer_wins:
-        print('You have won the best of {} games! What a champ :D'.format(n))
+        print('You won best of {} games! Good job'.format(n))
     else:
-        print('Unfortunately, the computer has won the best of {} games. Better luck next time!'.format(n))
-
-
+        print:('The computer has won best out of {} games. Try again later.'.format(n))
+        
 if __name__ == '__main__':
-    play_best_of(3) # 2
+    play_best_of(3)
